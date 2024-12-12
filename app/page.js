be47d8a62f07830b8b49e306967fa1b6bf2d0dfd
Rohan <rohan.webbies@gmail.com>
2024-12-12 19:46:34 +0530

    route changed

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@
 import React, { useEffect, useRef } from "react";
 import NetworkGraph from "./components/networkGraph/NetworkGraph";
 import data from "@/public/data.json";
+import ScrollAnimation from "./scrollAnimation/page";
 function Home() {
   return (
     <>
-      <div className="w-screen h-screen bg-white">
-        <NetworkGraph data={data} />
+      <div className="">
+        {/* <NetworkGraph data={data} /> */}
+        <ScrollAnimation />
       </div>
     </>
   );
